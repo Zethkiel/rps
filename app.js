5735846c43b93e9e2761424f9c77d8e81f95c792
Mar Joseph M. Bijer <70209257+Zethkiel@users.noreply.github.com>
2024-10-19 12:44:08 +0000

    integrating RPS to web

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const rps = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll("button");
+const results = document.querySelector(".results");
 
 function getComputerChoice() {
     return rps[Math.floor(Math.random() * rps.length)];
-}
-
-function getHumanChoice() {
-    return prompt("Rock, paper, or scissors?");
-}
+};
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (!humanChoice) return "There was no input.";
-    if (!rps.includes(humanChoice)) return "Invalid input!";
-    if (humanChoice === computerChoice) return "It's a tie!";
+    if (humanChoice === computerChoice) {
+        return results.textContent += "It's a tie!\r\n";  
+    };    
 
     const result = {
         rock: "scissors",
@@ -24,33 +22,31 @@ function playRound(humanChoice, computerChoice) {
 
     if (result[humanChoice] === computerChoice) {
         humanScore++;
-        return `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`;
+        results.textContent += `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.\r\n`;
     } else {
         computerScore++;
-        return `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`;
+        results.textContent += `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.\r\n`;
     }
-}
+};
 
 function playGame() {
-    let i = 0;
-    while (i < 5) {
-        const humanSelection = getHumanChoice().toLowerCase();
-        const computerSelection = getComputerChoice();
-        const result = playRound(humanSelection, computerSelection);
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            playRound(button.textContent.toLowerCase(), getComputerChoice());
 
-        if (result !== "Invalid input!" && result !== "There was no input.") {
-            i++;
-        }
-        console.log(result);
-    }
+            if (humanScore == 5 || computerScore == 5) {
+                results.textContent = `Human: ${humanScore}, Computer: ${computerScore}\r\n`;
 
-    console.log(`Human: ${humanScore}, Computer: ${computerScore}`);
+                results.textContent +=
+                humanScore > computerScore ? "Congrats! You won the game." :
+                humanScore < computerScore ? "You lost the game. Better luck next time!" :
+                "It's an overall tie!"
 
-    console.log(
-        humanScore > computerScore ? "Congrats! You won the game." :
-        humanScore < computerScore ? "You lost the game. Better luck next time!" :
-        "It's an overall tie!"
-    );
-}
+                humanScore = 0;
+                computerScore = 0;
+            }
+        })
+    })
+};
 
 playGame();
