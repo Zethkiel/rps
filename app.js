e2a393d9a3a89509720b23f88b4265e3ed51254a
Mar Joseph M. Bijer <70209257+Zethkiel@users.noreply.github.com>
2024-10-20 04:33:47 +0000

    clean up

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const rps = ["rock", "paper", "scissors"];
 const buttons = document.querySelectorAll("button");
 const results = document.querySelector(".results");
 
-function getComputerChoice() {
-    return rps[Math.floor(Math.random() * rps.length)];
-};
-
 let humanScore = 0;
 let computerScore = 0;
 
+function getComputerChoice() {
+    return rps[Math.floor(Math.random() * rps.length)];
+}
+
+function updateResults() {
+    results.textContent = `Human: ${humanScore}, Computer: ${computerScore}\r\n`;
+}
+
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        return results.textContent += "It's a tie!\r\n";  
+        updateResults();
+        results.textContent += "It's a tie!\r\n";  
+        return;
     };    
 
     const result = {
@@ -22,34 +28,43 @@ function playRound(humanChoice, computerChoice) {
 
     if (result[humanChoice] === computerChoice) {
         humanScore++;
-        results.textContent += `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.\r\n`;
+        updateResults();
+        results.textContent += `You win! ${capitalizeFirstLetter(humanChoice)} beats ${computerChoice}.\r\n`;
     } else {
         computerScore++;
-        results.textContent += `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.\r\n`;
+        updateResults();
+        results.textContent += `You lose! ${capitalizeFirstLetter(computerChoice)} beats ${humanChoice}.\r\n`;
     }
-};
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function resetScores() {
+    humanScore = 0;
+    computerScore = 0;
+    results.textContent = "";
+}
 
 function playGame() {
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
-            if (humanScore == 5 || computerScore == 5) {
-                humanScore = 0;
-                computerScore = 0;
-                results.textContent = "";
+            if (humanScore === 5 || computerScore === 5) {
+                resetScores();
             }
             
             playRound(button.textContent.toLowerCase(), getComputerChoice());
 
-            if (humanScore == 5 || computerScore == 5) {
-                results.textContent = `Human: ${humanScore}, Computer: ${computerScore}\r\n`;
-
-                results.textContent +=
-                humanScore > computerScore ? "Congrats! You won the game." :
-                humanScore < computerScore ? "You lost the game. Better luck next time!" :
-                "It's an overall tie!"
+            if (humanScore === 5 || computerScore === 5) {
+                if (humanScore > computerScore) {
+                    results.textContent += "Congrats! You won the game.";
+                } else if (humanScore < computerScore) {
+                    results.textContent += "You lost the game. Better luck next time!";
+                }
             }
         })
     })
-};
+}
 
 playGame();
